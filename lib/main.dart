3341ef86000040f138057e34fc3771b40bf54503
Zach Capshaw <zach@Zacharys-MacBook-Pro.local>
2022-06-19 09:40:42 -0700

    refactor home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:denari_mobile/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,104 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Denari Home Page'),
+      home: const HomeScreen(title: 'Denari Home Page'),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  late List<ChartData> _chartData;
-
-  List<ChartData> getChartData() {
-    final List<ChartData> chartData = [
-      ChartData('Spendable', 2486),
-    ];
-    return chartData;
-  }
-
-  @override
-  void initState() {
-    _chartData = getChartData();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You\'re in good shape ✅',
-              style: GoogleFonts.manrope(
-                  textStyle:
-                      TextStyle(color: Colors.blueGrey[900], fontSize: 20)),
-            ),
-            SfCircularChart(
-              annotations: <CircularChartAnnotation>[
-                CircularChartAnnotation(
-                  widget: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        '\$1,867',
-                        style: GoogleFonts.manrope(
-                          textStyle: TextStyle(
-                              color: Colors.blueGrey[900],
-                              fontSize: 50,
-                              fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                      Text(
-                        'Cash left to spend',
-                        style: GoogleFonts.manrope(
-                            textStyle: TextStyle(
-                                color: Colors.blueGrey[900], fontSize: 16)),
-                      ),
-                      Text(
-                        'This Month',
-                        style: GoogleFonts.manrope(
-                          textStyle: TextStyle(
-                            color: Colors.blueGrey[900],
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-              series: <CircularSeries>[
-                RadialBarSeries<ChartData, String>(
-                  dataSource: _chartData,
-                  xValueMapper: (ChartData data, _) => data.name,
-                  yValueMapper: (ChartData data, _) => data.value,
-                  maximumValue: 4000,
-                  pointColorMapper: (datum, index) => Colors.blueGrey[800],
-                  innerRadius: '90%',
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ChartData {
-  ChartData(this.name, this.value);
-  final String name;
-  final int value;
 }
